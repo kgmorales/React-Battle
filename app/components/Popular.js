dc70004733f19ed364abc1d461bb4d8a98df7990
gnarfugh <kmorales076@gmail.com>
2019-11-13 12:42:13 -0600

    caching api data to object

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -31,25 +31,34 @@ export default class Popular extends React.Component {
 
         this.state = {
             selectedLanguage: 'All',
-            repos: null,
+            repos: {},
             error: null
         }
 
         this.updateLanguage = this.updateLanguage.bind(this)
         this.isLoading = this.isLoading.bind(this)
     }
+    
+    componentDidMount() {
+        this.updateLanguage(this.state.selectedLanguage)
+    }
+
     updateLanguage (selectedLanguage) {
         this.setState({
             selectedLanguage,
-            repos: null,
             error: null
         })
 
-        fetchPopularRepos(selectedLanguage)
-            .then((repos) => this.setState({
-                repos,
-                error: null
-            }))
+        if (!this.state.repos[selectedLanguage]) {
+            fetchPopularRepos(selectedLanguage)
+            .then((data) => {
+                this.setState(({ repos }) => ({
+                    repos: {
+                        ...repos,
+                        [selectedLanguage]: data
+                    }
+                }))
+            })
             .catch(() => {
                 console.warn('Error fetching repos: ', error)
     
@@ -57,9 +66,12 @@ export default class Popular extends React.Component {
                     error: `There was an error fetching the repos.`
                 })
             })
+        }
     }
     isLoading() {
-        return this.state.repos === null && this.state.error === null
+        const { selectedLanguage, repos, error } = this.state
+        
+        return !repos[selectedLanguage] && error === null
     }
     render () {
         const { selectedLanguage, repos, error } = this.state
@@ -72,8 +84,10 @@ export default class Popular extends React.Component {
                 />
 
                 {this.isLoading() && <p>LOADING</p>}
+
                 {error && <p>{error}</p>}
-                {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+
+                {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
             </React.Fragment>
         )
     }
